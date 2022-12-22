67eb3f4a28fec8871b965069080f64fddf3df294
Vasyl Korol mac <gusak.guska223@gmail.com>
2022-12-22 14:51:02 +0100

    snake

---
+++ src/snake/Place.java
@@ -0,0 +1,38 @@
+package snake;
+
+public class Place {
+
+    private int[][] array;
+    private int y;
+    private int x;
+
+    public Place(int y, int x) {
+        this.y = y;
+        this.x = x;
+        this.array = new int[y][x];
+    }
+
+    public int[][] getArray() {
+        return array;
+    }
+
+    public void setArray(int[][] array) {
+        this.array = array;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
---
+++ src/snake/SnakeController.java
@@ -0,0 +1,38 @@
+package snake;
+
+import java.util.Scanner;
+
+public class SnakeController implements Runnable{
+    private volatile Snake snake;
+
+    public SnakeController(Snake snake) {
+        this.snake = snake;
+    }
+
+    private final Scanner scanner = new Scanner(System.in);
+
+
+    @Override
+    public void run() {
+        while (true){
+            var guess = scanner.nextLine();
+            String vector = snake.getRunVector();
+            if (guess.equals("w") && !vector.equals("s")){
+                snake.setRunVector("w");
+            }
+            else if (guess.equals("s") && !vector.equals("w")){
+                snake.setRunVector("s");
+            }
+            else if (guess.equals("a") && !vector.equals("d")){
+                snake.setRunVector("a");
+            }
+            else if (guess.equals("d") && !vector.equals("a")){
+                snake.setRunVector("d");
+            }
+            else {
+                snake.grow();
+            }
+        }
+
+    }
+}
